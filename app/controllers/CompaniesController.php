f32537c77e56d507c2aa15197e48b8c898df3134
MITAAkiko <mita@ye-p.co.jp>
2022-05-31 22:59:40 +0900

    会社一覧の上のphp文をcontrollerのクラスで

--- app/controllers/CompaniesController.php
+++ app/controllers/CompaniesController.php
@@ -1,34 +1,48 @@
 <?php
 namespace App\Controllers;
+// require_once('../app/models/CompaniesModel.php');
+// use App\Models\CompaniesModel;
 
 //require_once('../../dbconnect.php');
 class CompaniesController
 {
+
+    // private $cmpMdl;
+    private $db;
     public function __construct()
     {
-        require_once('../../dbconnect.php');
-        $this->db = $db;
+        // $this->cmpMdl = new CompaniesModel;
+        $this->db = new \PDO('mysql:dbname=programming_training;host=127.0.0.1;charset=utf8', 'root', 'P@ssw0rd');
     }
-    // public function db()
-    // {
-    //     //require_once('../../dbconnect.php');
-    //     $user='root';
-    //     $pass='P@ssw0rd';
-    //     try {
-    //         $db = new PDO('mysql:dbname=programming_training;host=127.0.0.1;charset=utf8', $user, $pass);
-    //         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //     } catch (PDOException $e) {
-    //         echo '接続エラー:'.$e->getMessage();
-    //     }
-    //     return $db;
-    // }
-
+    
     public function index($get, $post = null)
     {
-        require_once('../../dbconnect.php');
-            //初期値
+        
+        //初期値
         if (empty($get['order'])) {
             $get['order']=1;
+        }
+
+
+        //maxPageを取得する
+        if (!empty($get['search'])) {
+            $searched = '%'.$get['search'].'%' ;
+            $counts = $this->db->prepare('SELECT COUNT(*) AS cnt FROM companies WHERE deleted IS NULL AND (company_name LIKE ? OR manager_name LIKE ?)');
+            $counts->bindParam(1, $searched, \PDO::PARAM_STR);
+            $counts->bindParam(2, $searched, \PDO::PARAM_STR);
+            $counts->execute();
+            $cnt = $counts->fetch();
+            
+            // $res = $this->cmpMdl->getMaxPageSearched($get);
+            // $cnt = $res['cnt'];
+            $maxPage = ceil($cnt['cnt']/10);
+        } else {
+            $counts = $this->db->prepare('SELECT COUNT(*) AS cnt FROM companies WHERE deleted IS NULL');
+            $counts->execute();
+            $cnt = $counts->fetch();
+            // $res = $this->cmpMdl->getMaxPage();
+            // $cnt = $res['cnt'];
+            $maxPage = ceil($cnt['cnt']/10);
         }
 
         $page = 0;
@@ -37,69 +51,55 @@ class CompaniesController
             if ($page == '') {
                 $page=1;
             }
+        } else {
+            $page = 1;
         }
         //最小値
         $page = max($page, 1);
-        //最後のページを取得する
-
-        if (!empty($get['search'])) {
-            $searched = '%'.$get['search'].'%' ;
-            $counts = $this->db->prepare('SELECT COUNT(*) AS cnt FROM companies WHERE deleted IS NULL AND (company_name LIKE ? OR manager_name LIKE ?)');
-            $counts->bindParam(1, $searched, PDO::PARAM_STR);
-            $counts->bindParam(2, $searched, PDO::PARAM_STR);
-            $counts->execute();
-            $cnt = $counts->fetch();
-            $maxPage = ceil($cnt['cnt']/10);
-        } else {
-            $counts = $this->db->query('SELECT COUNT(*) AS cnt FROM companies WHERE deleted IS NULL');
-            $cnt = $counts->fetch();
-            $maxPage = ceil($cnt['cnt']/10);
-        }
-
         //最大値
         $page = min($page, $maxPage);
         //ページ
         $start = ($page - 1) * 10;
 
         //DBに接続する用意
-
         if (!empty($get['search'])) {//GETでおくる
             if (($get['order'])>0) {
                 $searched = '%'.$get['search'].'%' ;
                 $companies=$this->db->prepare('SELECT id, company_name, manager_name, phone_number, postal_code, prefecture_code, address, mail_address 
                     FROM companies WHERE deleted IS NULL AND (company_name LIKE ? OR manager_name LIKE ?)
                     ORDER BY id ASC LIMIT ?,10');
-                $companies->bindParam(1, $searched, PDO::PARAM_STR);
-                $companies->bindParam(2, $searched, PDO::PARAM_STR);
-                $companies->bindParam(3, $start, PDO::PARAM_INT);
+                $companies->bindParam(1, $searched, \PDO::PARAM_STR);
+                $companies->bindParam(2, $searched, \PDO::PARAM_STR);
+                $companies->bindParam(3, $start, \PDO::PARAM_INT);
                 $companies->execute();
             } else {
                 $searched = '%'.$get['search'].'%' ;
                 $companies=$this->db->prepare('SELECT id, company_name, manager_name, phone_number, postal_code, prefecture_code, address, mail_address 
                     FROM companies WHERE deleted IS NULL AND (company_name LIKE ? OR manager_name LIKE ?)
                     ORDER BY id DESC LIMIT ?,10');
-                $companies->bindParam(1, $searched, PDO::PARAM_STR);
-                $companies->bindParam(2, $searched, PDO::PARAM_STR);
-                $companies->bindParam(3, $start, PDO::PARAM_INT);
+                $companies->bindParam(1, $searched, \PDO::PARAM_STR);
+                $companies->bindParam(2, $searched, \PDO::PARAM_STR);
+                $companies->bindParam(3, $start, \PDO::PARAM_INT);
                 $companies->execute();
             }
         } else {//検索なかった場合
             if (($get['order'])>0) {
                 $companies=$this->db->prepare('SELECT id, company_name, manager_name, phone_number, postal_code, prefecture_code, address, mail_address 
                     FROM companies WHERE deleted IS NULL ORDER BY id ASC LIMIT ?,10');
-                $companies->bindParam(1, $start, PDO::PARAM_INT);
+                $companies->bindParam(1, $start, \PDO::PARAM_INT);
                 $companies->execute();
             } else {
                 $companies=$this->db->prepare('SELECT id, company_name, manager_name, phone_number, postal_code, prefecture_code, address, mail_address 
                     FROM companies WHERE deleted IS NULL ORDER BY id DESC LIMIT ?,10');
-                $companies->bindParam(1, $start, PDO::PARAM_INT);
+                $companies->bindParam(1, $start, \PDO::PARAM_INT);
                 $companies->execute();
             }
         }
 
         return [
             'companies' => $companies,
-            
+            'maxPage' => $maxPage,
+            'page' => $page,
         ];
     }
 }
--- companies/index.php
+++ companies/index.php
@@ -6,39 +6,39 @@ require_once('../functions.php');
 require_once('../app/controllers/CompaniesController.php');
 
 //初期値
-if (empty($_GET['order'])) {
-    $_GET['order']=1;
-}
+// if (empty($_GET['order'])) {
+//     $_GET['order']=1;
+// }
 
-$page = 0;
-if (!empty($_GET['page'])) {
-    $page= $_GET['page'];
-    if ($page == '') {
-        $page=1;
-    }
-}
-//最小値
-$page = max($page, 1);
-//最後のページを取得する
+// $page = 0;
+// if (!empty($_GET['page'])) {
+//     $page= $_GET['page'];
+//     if ($page == '') {
+//         $page=1;
+//     }
+// }
+// //最小値
+// $page = max($page, 1);
+// //最後のページを取得する
 
-if (!empty($_GET['search'])) {
-    $searched = '%'.$_GET['search'].'%' ;
-    $counts = $db->prepare('SELECT COUNT(*) AS cnt FROM companies WHERE deleted IS NULL AND (company_name LIKE ? OR manager_name LIKE ?)');
-    $counts->bindParam(1, $searched, PDO::PARAM_STR);
-    $counts->bindParam(2, $searched, PDO::PARAM_STR);
-    $counts->execute();
-    $cnt = $counts->fetch();
-    $maxPage = ceil($cnt['cnt']/10);
-} else {
-    $counts = $db->query('SELECT COUNT(*) AS cnt FROM companies WHERE deleted IS NULL');
-    $cnt = $counts->fetch();
-    $maxPage = ceil($cnt['cnt']/10);
-}
+// if (!empty($_GET['search'])) {
+//     $searched = '%'.$_GET['search'].'%' ;
+//     $counts = $db->prepare('SELECT COUNT(*) AS cnt FROM companies WHERE deleted IS NULL AND (company_name LIKE ? OR manager_name LIKE ?)');
+//     $counts->bindParam(1, $searched, PDO::PARAM_STR);
+//     $counts->bindParam(2, $searched, PDO::PARAM_STR);
+//     $counts->execute();
+//     $cnt = $counts->fetch();
+//     $maxPage = ceil($cnt['cnt']/10);
+// } else {
+//     $counts = $db->query('SELECT COUNT(*) AS cnt FROM companies WHERE deleted IS NULL');
+//     $cnt = $counts->fetch();
+//     $maxPage = ceil($cnt['cnt']/10);
+// }
 
-//最大値
-$page = min($page, $maxPage);
+// //最大値
+// $page = min($page, $maxPage);
 //ページ
-$start = ($page - 1) * 10;
+// $start = ($page - 1) * 10;
 
 //DBに接続する用意
 //検索した場合
@@ -81,6 +81,10 @@ $start = ($page - 1) * 10;
  $cmp = new CompaniesController;
  $res = $cmp->index($_GET);
  $companies = $res['companies'];
+ $maxPage = $res['maxPage'];
+ $page = $res['page'];
+
+            
 
 // use App\Controllers\CompaniesController;
 
