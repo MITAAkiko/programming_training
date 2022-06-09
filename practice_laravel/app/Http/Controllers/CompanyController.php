f17016bec41875680bce6eb2c1ac7a8a0f672508
MITAAkiko <mita@ye-p.co.jp>
2022-06-09 12:01:20 +0900

    バリデーションチェックOK。INSERTできず

--- practice_laravel/app/Http/Controllers/CompanyController.php
+++ practice_laravel/app/Http/Controllers/CompanyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;//getやpostを受け取れる？
 use App\Models\Company;
+use Illuminate\Support\Facades\Validator;
 
 class CompanyController extends Controller
 {
@@ -43,5 +44,39 @@ class CompanyController extends Controller
 
         $prefecture = config('config.PREFECTURES');
         return view('add', compact('prefecture', $posts));
+    }
+
+    public function validation(Request $post)
+    {
+        $validator = Validator::make($post->all(), [//第1引数input　第２引数ルール　第３引数メッセージ　第４引数:attributeの中に入れる文字の設定
+            'name' => 'required|max:64', //空文字、64文字
+            'manager' => 'required|max:32',
+            'phone' => 'required|digits_between:1,11',
+            'postal' => 'required|digits:7',
+            'prefecture_code' => 'required|max:47|min:1',//エラー文字登録していない
+            'address' => 'required|max:100',
+            'email' => 'required|email:rfc,dns',
+            'prefix' => 'required|alpha_num|max:16'//一旦全角OK
+        ], [
+            'required' => '入力してください',
+            'max' => ':max 文字で入力してください',
+            'integer' => '数字で入力してください',
+            'digits' => ':digits 桁の数字で入力してください',
+            'size' => ':size 文字で入力してください',
+            'email' => '正しく入力してください',
+            'alpha_num' => '英数字で入寮してください',
+            'digits_between' => '11桁以内で入力してください',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/add')
+            ->withErrors($validator, 'add')//addという名前を付けると$errors->add->で使える
+            ->withInput();//oldにいれる
+        } else {
+            $validated = $post->validated();
+            $validated = $post->safe()->all();//これをSQLに登録
+            $this->cmpMdl->create($validated);
+            dd($validated);
+            return view('index');
+        }
     }
 }
--- practice_laravel/app/Models/Company.php
+++ practice_laravel/app/Models/Company.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Company extends Model
 {
@@ -37,5 +38,31 @@ class Company extends Model
             ;
               //  ->get();
         return $datas;
+    }
+    public function create($value)
+    {
+        DB::table('companies')->insert([
+            
+            'company_name' => $value['name'],
+            'manager_name' => $value['name'],
+            'phone_number' => $value['phone'],
+            'postal_code' => $value['postal'],
+            'prefecture_code' => $value['prefecture_code'],
+            'address' => $value['address'],
+            'mail_address' => $value['email'],
+            'prefix' => $value['prefix'],
+            'created' => NOW(),
+            'modified' => NOW()
+        ]);
+        $statement = $this->db->prepare('INSERT INTO companies SET company_name=?, manager_name=?,phone_number=?,postal_code=?,prefecture_code=?,address=?,mail_address=?,prefix=?,created=NOW(),modified=NOW()');
+        $statement->bindParam(1, $value['name'], \PDO::PARAM_STR);
+        $statement->bindParam(2, $value['name'], \PDO::PARAM_STR);
+        $statement->bindParam(3, $value['phone'], \PDO::PARAM_STR);
+        $statement->bindParam(4, $value['postal'], \PDO::PARAM_STR);
+        $statement->bindParam(5, $value['prefecture_code'], \PDO::PARAM_STR);
+        $statement->bindParam(6, $value['address'], \PDO::PARAM_STR);
+        $statement->bindParam(7, $value['email'], \PDO::PARAM_STR);
+        $statement->bindParam(8, $value['prefix'], \PDO::PARAM_STR);
+        $statement->execute();
     }
 }
