197c1996937598530cbf32e0422586b1ac41b19f
MITAAkiko <mita@ye-p.co.jp>
2022-06-18 07:41:25 +0900

    検索時、あてはまるデータがないとPDOの方でエラーになるため、try/catch削除

--- app/models/CompaniesModel.php
+++ app/models/CompaniesModel.php
@@ -8,12 +8,7 @@ class CompaniesModel
     {
         $user = 'root';
         $pass = 'P@ssw0rd';
-        try {
-            $this->db = new \PDO('mysql:dbname=programming_training;host=127.0.0.1;charset=utf8', $user, $pass);
-            $this->db -> setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        } catch (\PDOException $e) {
-            echo '接続エラー:'.$e -> getMessage();
-        }
+        $this->db = new \PDO('mysql:dbname=programming_training;host=127.0.0.1;charset=utf8', $user, $pass);
     }
     public function fetchMaxPageSearched($searched)
     {
